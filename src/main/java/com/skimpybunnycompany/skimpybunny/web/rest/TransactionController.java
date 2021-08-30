b48c52148d391b873bf222a9d084858b1e526505
romanpindela <roman.pindela@gmail.com>
2021-08-30 17:22:22 +0200

    Aded inserts of test transactions for another user

--- src/main/java/com/skimpybunnycompany/skimpybunny/web/rest/TransactionController.java
+++ src/main/java/com/skimpybunnycompany/skimpybunny/web/rest/TransactionController.java
@@ -2,6 +2,7 @@ package com.skimpybunnycompany.skimpybunny.web.rest;
 
 import com.skimpybunnycompany.skimpybunny.domain.Transaction;
 import com.skimpybunnycompany.skimpybunny.response.TransactionResponse;
+import com.skimpybunnycompany.skimpybunny.security.SecurityUtils;
 import com.skimpybunnycompany.skimpybunny.service.TransactionService;
 import com.sun.xml.bind.v2.TODO;
 import java.util.List;
@@ -17,11 +18,10 @@ public class TransactionController {
     @Autowired
     TransactionService transactionService;
 
-    // TODO: /transactions/{user}/{PAGE_NR} - GET
+    // TODO: /transactions/{PAGE_NR} - GET
     @GetMapping
     public List<TransactionResponse> getAllTransactions() {
         List<TransactionResponse> transactionsResponse = transactionService.getAllTransactions();
-        System.out.println(transactionsResponse);
         return transactionsResponse;
     }
     // TODO: /transactions/{user}/{transaction_id} - PATCH, DELETE, GET
