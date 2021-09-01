13706eb767a9be99bfca7e942f38daeb7adcca43
romanpindela <roman.pindela@gmail.com>
2021-09-01 22:17:02 +0200

    Fixed exceptions handling with request to api and to endpoint /api/transaction

--- src/main/java/com/skimpybunnycompany/skimpybunny/exception/ApiExceptionHandler.java
+++ src/main/java/com/skimpybunnycompany/skimpybunny/exception/ApiExceptionHandler.java
@@ -4,9 +4,11 @@ import java.time.ZoneId;
 import java.time.ZonedDateTime;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
+@ControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(value = { ApiRequestException.class })
