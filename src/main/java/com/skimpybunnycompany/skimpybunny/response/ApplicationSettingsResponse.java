d537da32da5cb8a6e6548a1669deccb57eeaa0ef
romanpindela <roman.pindela@gmail.com>
2021-09-17 21:29:19 +0200

    Response scope extension for GET / api / applicationsettings

--- src/main/java/com/skimpybunnycompany/skimpybunny/response/ApplicationSettingsResponse.java
+++ src/main/java/com/skimpybunnycompany/skimpybunny/response/ApplicationSettingsResponse.java
@@ -25,13 +25,49 @@ public class ApplicationSettingsResponse {
     @ApiModelProperty(value = "Currency name for transactions", position = 4, example = "PLN, EUR, USD")
     private String currencyName;
 
+    @ApiModelProperty(value = "isNotificationActive", position = 4, example = "true or false")
+    private Boolean isNotificationActive;
+
+    @ApiModelProperty(value = "isActiveEmailRaport", position = 4, example = "true or false")
+    private Boolean isActiveEmailRaport;
+
+    @ApiModelProperty(value = "isPaidSubscription", position = 4, example = "true or false")
+    private Boolean isPaidSubscription;
+
     public ApplicationSettingsResponse(ApplicationSettings as) {
         this.themeName = as.getThemeName();
         this.currencyName = as.getCurrencyName();
         this.languageName = as.getLanguageName();
+        this.isNotificationActive = as.getNotificationActive();
+        this.isActiveEmailRaport = as.getActiveEmailRaport();
+        this.isPaidSubscription = as.getPaidSubscription();
     }
 
     public ApplicationSettingsResponse() {}
+
+    public Boolean getNotificationActive() {
+        return isNotificationActive;
+    }
+
+    public void setNotificationActive(Boolean notificationActive) {
+        isNotificationActive = notificationActive;
+    }
+
+    public Boolean getActiveEmailRaport() {
+        return isActiveEmailRaport;
+    }
+
+    public void setActiveEmailRaport(Boolean activeEmailRaport) {
+        isActiveEmailRaport = activeEmailRaport;
+    }
+
+    public Boolean getPaidSubscription() {
+        return isPaidSubscription;
+    }
+
+    public void setPaidSubscription(Boolean paidSubscription) {
+        isPaidSubscription = paidSubscription;
+    }
 
     public String getThemeName() {
         return themeName;
@@ -70,6 +106,12 @@ public class ApplicationSettingsResponse {
             ", currencyName='" +
             currencyName +
             '\'' +
+            ", isNotificationActive=" +
+            isNotificationActive +
+            ", isActiveEmailRaport=" +
+            isActiveEmailRaport +
+            ", isPaidSubscription=" +
+            isPaidSubscription +
             '}'
         );
     }
